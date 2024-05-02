2f66c772c3f5064971629a4f442e0aba32ddcd7f
0122345 <ntwarifiacre043@gmail.com>
2024-05-02 11:54:24 +0200

    initiall commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+ /** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./components/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+         nav: "#FBF6F2"
+      }
+    },
+    
+  },
+  plugins: [],
+}
